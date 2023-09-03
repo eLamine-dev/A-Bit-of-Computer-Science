beee6569b142a7884ae3bf489019f2172da574a6
e-lamine <lamine@e-lamine.dev>
2023-09-03 22:15:42 +0100

    working list

--- 3-Linked Lists /LinkedList.js
+++ 3-Linked Lists /LinkedList.js
@@ -33,16 +33,13 @@ const LinkedList = function () {
    };
    //    size returns the total number of nodes in the list
    list.size = () => {
-      let size = 0;
       const helper = (node) => {
-         if (node.nextNode === null) {
-            return ++size;
+         if (node === null) {
+            return 0;
          }
-         size++;
-         return helper(node.nextNode);
+         return 1 + helper(node.nextNode);
       };
 
-      if (list.head === null) return 0;
       return helper(list.head);
    };
    //    head returns the first node in the list
@@ -53,93 +50,124 @@ const LinkedList = function () {
    //    tail returns the last node in the list
    list.tail = () => {
       const helper = (node) => {
-         if (node.nextNode === null) {
+         if (node.nextNode === null || node === null) {
             return node;
-         } else return helper(node.nextNode);
+         }
+         return helper(node.nextNode);
       };
 
-      if (list.head === null) return 'Empty List';
-      else return helper(list.head);
+      return helper(list.head);
    };
 
    //    at(index) returns the node at the given index
    list.at = (index) => {
-      const helper = (node) => {
-         for (let i = 0; i < index; i++) {
-            node = node.nextNode;
-            if (node === null) return 'No node at this index';
-         }
+      const helper = (node, i = 0) => {
+         if (node === null) return 'No node at this index';
+         if (index === i) return node;
 
-         return node;
+         return helper(node.nextNode, i + 1);
       };
 
-      if (list.head === null) return 'Empty List';
-      else return helper(list.head);
+      return helper(list.head);
    };
 
    //    pop removes the last element from the list
 
    list.pop = () => {
-      let previous = null;
-      const helper = (node) => {
-         if (node.nextNode === null) {
-            if (node === list.head) list.head = null;
-            return;
-         } else {
-            previous = node;
-            helper(node.nextNode);
-         }
-         previous.nextNode = null;
-      };
-
       if (list.head === null) return 'Empty List';
-      else helper(list.head);
+      if (list.head.nextNode === null) {
+         list.head = null;
+      } else {
+         const helper = (node) => {
+            if (node.nextNode.nextNode === null) {
+               node.nextNode = null;
+               return;
+            }
+            helper(node.nextNode);
+         };
+
+         helper(list.head);
+      }
    };
    //    contains(value) returns true if the passed in value is in the list and otherwise returns false.
    list.contains = (value) => {
       const helper = (node) => {
          if (node.value === value) return true;
-         if (node.nextNode === null) return false;
+         if (node === null) return false;
 
          return helper(node.nextNode);
       };
 
-      if (list.head === null) return 'Empty list';
       return helper(list.head);
    };
 
    //    find(value) returns the index of the node containing value, or null if not found.
    list.find = (value) => {
-      let index = 0;
-      const helper = (node) => {
-         if (node.value === value) return index;
-         index++;
-         return helper(node.nextNode);
+      const helper = (node, i = 0) => {
+         if (node === null) return 'Not found';
+         if (node.value === value) return i;
+
+         return helper(node.nextNode, i + 1);
       };
 
-      if (list.head === null) return 'Empty list';
       return helper(list.head);
    };
    //    toString represents your LinkedList objects as strings, so you can print them out and preview them in the      console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
 
    list.toString = () => {
-      let string = '';
       const helper = (node) => {
-         if (node.nextNode === null)
-            return string.concat(` (${node.value}) -> ${node.nextNode}`);
-         else {
-            return string
-               .concat(` (${node.value}) -> `)
-               .concat(helper(node.nextNode));
-         }
+         if (node === null) return 'null';
+
+         return `(${node.value}) -> ${helper(node.nextNode)}`;
       };
 
-      if (list.head === null) return 'Empty List';
-      else return helper(list.head);
+      return helper(list.head);
    };
 
    //    insertAt(value, index) that inserts a new node with the provided value at the given index.
+
+   list.insertAt = (value, index) => {
+      let newNode = createNode(value);
+      if (index === 0) {
+         newNode.nextNode = list.head;
+         list.head = newNode;
+         return;
+      }
+
+      const helper = (node, i) => {
+         if (i + 1 === index) {
+            newNode.nextNode = node.nextNode;
+            node.nextNode = newNode;
+            return;
+         }
+         helper(node.nextNode, i + 1);
+      };
+
+      return helper(list.head, 0);
+   };
+
    //    removeAt(index) that removes the node at the given index.
+
+   list.removeAt = (index) => {
+      if (list.head === null) return 'Empty List';
+      else if (index === 0) list.head = list.head.nextNode;
+      else {
+         const helper = (node, i) => {
+            if (node.nextNode === null) {
+               console.log('index provided is out of bounds');
+               return;
+            }
+            if (i + 1 === index) {
+               node.nextNode = node.nextNode.nextNode;
+               return;
+            }
+
+            helper(node.nextNode, i + 1);
+         };
+
+         return helper(list.head, 0);
+      }
+   };
 
    return list;
 };
@@ -166,19 +194,27 @@ myList.append('fifth node');
 console.log(myList.size());
 
 console.log(myList.tail());
-// console.log(myList.pop());
-// console.log(myList.pop());
-// console.log(myList.pop());
-// console.log(myList.pop());
-// console.log(myList.pop());
-// console.log(myList.pop());
 
 console.log(myList.tail());
 
 console.log(myList.size());
 
 console.log(myList.contains('fifth node'));
-
+console.log(myList.at(6));
 console.log(myList.find('0 index node'));
+
+myList.removeAt(5);
+// myList.insertAt('inserted node', 2);
+
+myList.pop();
+myList.pop();
+myList.pop();
+myList.pop();
+myList.pop();
+myList.pop();
+
+console.log(myList.toString());
+
+myList.insertAt('inserted to empty list', 0);
 
 console.log(myList.toString());
