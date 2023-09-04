e6ccaa54ad50d7ce51ed99e89af6fa5080e77174
e-lamine <lamine@e-lamine.dev>
2023-09-04 09:51:59 +0100

    rewrote the linked list with non recursive methods

--- 3-Linked Lists /LinkedList.js
+++ 3-Linked Lists /LinkedList.js
@@ -5,11 +5,11 @@ const createNode = function (value) {
    };
 };
 
+// linked list using non recursive methods
+
 const LinkedList = function () {
    const list = {};
    list.head = null;
-
-   //    append(value) adds a new node containing value to the end of the list
 
    list.append = (value) => {
       let newNode = createNode(value);
@@ -20,113 +20,97 @@ const LinkedList = function () {
       }
    };
 
-   //    prepend(value) adds a new node containing value to the start of the list
    list.prepend = (value) => {
       let newNode = createNode(value);
       if (list.head === null) {
          list.head = newNode;
       } else {
-         let temp = list.head;
+         newNode.nextNode = list.head;
          list.head = newNode;
-         list.head.nextNode = temp;
       }
    };
-   //    size returns the total number of nodes in the list
+
    list.size = () => {
-      const helper = (node) => {
-         if (node === null) {
-            return 0;
-         }
-         return 1 + helper(node.nextNode);
-      };
-
-      return helper(list.head);
+      let count = 0;
+      let currentNode = list.head;
+      while (currentNode != null) {
+         currentNode = currentNode.nextNode;
+         count++;
+      }
+      return count;
    };
-   //    head returns the first node in the list
 
-   list.listHead = () => {
-      return list.head;
-   };
-   //    tail returns the last node in the list
    list.tail = () => {
-      const helper = (node) => {
-         if (node.nextNode === null || node === null) {
-            return node;
-         }
-         return helper(node.nextNode);
-      };
+      let currentNode = list.head;
+      while (currentNode.nextNode != null) {
+         currentNode = currentNode.nextNode;
+      }
 
-      return helper(list.head);
+      return currentNode;
    };
 
-   //    at(index) returns the node at the given index
    list.at = (index) => {
-      const helper = (node, i = 0) => {
-         if (node === null) return 'No node at this index';
-         if (index === i) return node;
+      if (index < 0) return 'Invalid index';
+      let currentNode = list.head;
+      let i = 0;
+      while (currentNode && i < index) {
+         currentNode = currentNode.nextNode;
+         i++;
+      }
 
-         return helper(node.nextNode, i + 1);
-      };
-
-      return helper(list.head);
+      return currentNode || 'index provided is out of bounds';
    };
-
-   //    pop removes the last element from the list
 
    list.pop = () => {
-      if (list.head === null) return 'Empty List';
-      if (list.head.nextNode === null) {
+      if (list.head === null) {
+         console.log('Empty List');
+      } else if (list.head.nextNode === null) {
          list.head = null;
       } else {
-         const helper = (node) => {
-            if (node.nextNode.nextNode === null) {
-               node.nextNode = null;
-               return;
-            }
-            helper(node.nextNode);
-         };
-
-         helper(list.head);
+         let currentNode = list.head;
+         while (currentNode.nextNode && currentNode.nextNode.nextNode) {
+            currentNode = currentNode.nextNode;
+         }
+         currentNode.nextNode = null;
       }
    };
-   //    contains(value) returns true if the passed in value is in the list and otherwise returns false.
+
    list.contains = (value) => {
-      const helper = (node) => {
-         if (node.value === value) return true;
-         if (node === null) return false;
+      let currentNode = list.head;
+      while (currentNode != null) {
+         if (currentNode.value === value) return true;
+         currentNode = currentNode.nextNode;
+      }
 
-         return helper(node.nextNode);
-      };
-
-      return helper(list.head);
+      return false;
    };
 
-   //    find(value) returns the index of the node containing value, or null if not found.
    list.find = (value) => {
-      const helper = (node, i = 0) => {
-         if (node === null) return 'Not found';
-         if (node.value === value) return i;
+      let currentNode = list.head;
+      let i = 0;
+      while (currentNode != null) {
+         if (currentNode.value === value) return i;
+         currentNode = currentNode.nextNode;
+         i++;
+      }
 
-         return helper(node.nextNode, i + 1);
-      };
-
-      return helper(list.head);
+      return 'Not Found';
    };
-   //    toString represents your LinkedList objects as strings, so you can print them out and preview them in the      console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
 
    list.toString = () => {
-      const helper = (node) => {
-         if (node === null) return 'null';
+      let string = '';
+      let currentNode = list.head;
 
-         return `(${node.value}) -> ${helper(node.nextNode)}`;
-      };
+      while (currentNode != null) {
+         string += `( ${currentNode.value} ) -> `;
+         currentNode = currentNode.nextNode;
+      }
 
-      return helper(list.head);
+      return (string += 'null');
    };
 
-   //    insertAt(value, index) that inserts a new node with the provided value at the given index.
-
    list.insertAt = (value, index) => {
+      if (index < 0) return 'Invalid index';
       let newNode = createNode(value);
       if (index === 0) {
          newNode.nextNode = list.head;
@@ -134,39 +118,42 @@ const LinkedList = function () {
          return;
       }
 
-      const helper = (node, i) => {
-         if (i + 1 === index) {
-            newNode.nextNode = node.nextNode;
-            node.nextNode = newNode;
+      let currentNode = list.head;
+      let i = 0;
+      while (currentNode) {
+         if (i === index - 1) {
+            newNode.nextNode = currentNode.nextNode;
+            currentNode.nextNode = newNode;
             return;
          }
-         helper(node.nextNode, i + 1);
-      };
-
-      return helper(list.head, 0);
+         i++;
+         currentNode = currentNode.nextNode;
+      }
+      console.log('Invalid index');
    };
 
-   //    removeAt(index) that removes the node at the given index.
-
    list.removeAt = (index) => {
+      if (index < 0) return 'Invalid index';
       if (list.head === null) return 'Empty List';
-      else if (index === 0) list.head = list.head.nextNode;
-      else {
-         const helper = (node, i) => {
-            if (node.nextNode === null) {
-               console.log('index provided is out of bounds');
-               return;
-            }
-            if (i + 1 === index) {
-               node.nextNode = node.nextNode.nextNode;
-               return;
-            }
-
-            helper(node.nextNode, i + 1);
-         };
-
-         return helper(list.head, 0);
+      else if (index === 0) {
+         list.head = list.head.nextNode;
+         return;
       }
+
+      let currentNode = list.head;
+      let i = 0;
+      while (currentNode) {
+         if (i === index - 1) {
+            if (!currentNode.nextNode) {
+               return 'Index out of bounds';
+            }
+            currentNode.nextNode = currentNode.nextNode.nextNode;
+            return;
+         }
+         i++;
+         currentNode = currentNode.nextNode;
+      }
+      console.log('No node at this index');
    };
 
    return list;
@@ -199,12 +186,12 @@ console.log(myList.tail());
 
 console.log(myList.size());
 
-console.log(myList.contains('fifth node'));
-console.log(myList.at(6));
-console.log(myList.find('0 index node'));
+console.log(myList.contains('third node'));
+console.log(myList.at(3));
+console.log(myList.find('fifth node'));
 
-myList.removeAt(5);
-// myList.insertAt('inserted node', 2);
+myList.removeAt(7);
+myList.insertAt('inserted node', 2);
 
 myList.pop();
 myList.pop();
@@ -212,9 +199,164 @@ myList.pop();
 myList.pop();
 myList.pop();
 myList.pop();
-
+myList.pop();
 console.log(myList.toString());
 
 myList.insertAt('inserted to empty list', 0);
 
 console.log(myList.toString());
+
+// linked list using recursive methods
+
+const LinkedListRecursiveMethods = function () {
+   const list = {};
+   list.head = null;
+
+   list.append = (value) => {
+      let newNode = createNode(value);
+      if (list.head === null) {
+         list.head = newNode;
+      } else {
+         list.tail().nextNode = newNode;
+      }
+   };
+
+   list.prepend = (value) => {
+      let newNode = createNode(value);
+      if (list.head === null) {
+         list.head = newNode;
+      } else {
+         newNode.nextNode = list.head;
+         list.head = newNode;
+      }
+   };
+
+   list.size = () => {
+      const helper = (node) => {
+         if (node === null) {
+            return 0;
+         }
+         return 1 + helper(node.nextNode);
+      };
+
+      return helper(list.head);
+   };
+
+   list.listHead = () => {
+      return list.head;
+   };
+
+   list.tail = () => {
+      const helper = (node) => {
+         if (node.nextNode === null || node === null) {
+            return node;
+         }
+         return helper(node.nextNode);
+      };
+
+      return helper(list.head);
+   };
+
+   list.at = (index) => {
+      const helper = (node, i = 0) => {
+         if (node === null) return 'No node at this index';
+         if (index === i) return node;
+
+         return helper(node.nextNode, i + 1);
+      };
+
+      return helper(list.head);
+   };
+
+   list.pop = () => {
+      if (list.head === null) return 'Empty List';
+      if (list.head.nextNode === null) {
+         list.head = null;
+      } else {
+         const helper = (node) => {
+            if (node.nextNode.nextNode === null) {
+               node.nextNode = null;
+               return;
+            }
+            helper(node.nextNode);
+         };
+
+         helper(list.head);
+      }
+   };
+
+   list.contains = (value) => {
+      const helper = (node) => {
+         if (node.value === value) return true;
+         if (node === null) return false;
+
+         return helper(node.nextNode);
+      };
+
+      return helper(list.head);
+   };
+
+   list.find = (value) => {
+      const helper = (node, i = 0) => {
+         if (node === null) return 'Not found';
+         if (node.value === value) return i;
+
+         return helper(node.nextNode, i + 1);
+      };
+
+      return helper(list.head);
+   };
+
+   list.toString = () => {
+      const helper = (node) => {
+         if (node === null) return 'null';
+
+         return `(${node.value}) -> ${helper(node.nextNode)}`;
+      };
+
+      return helper(list.head);
+   };
+
+   list.insertAt = (value, index) => {
+      let newNode = createNode(value);
+      if (index === 0) {
+         newNode.nextNode = list.head;
+         list.head = newNode;
+         return;
+      }
+
+      const helper = (node, i) => {
+         if (i + 1 === index) {
+            newNode.nextNode = node.nextNode;
+            node.nextNode = newNode;
+            return;
+         }
+         helper(node.nextNode, i + 1);
+      };
+
+      return helper(list.head, 0);
+   };
+
+   list.removeAt = (index) => {
+      if (list.head === null) return 'Empty List';
+      else if (index === 0) list.head = list.head.nextNode;
+      else {
+         const helper = (node, i) => {
+            if (node.nextNode === null) {
+               console.log('index provided is out of bounds');
+               return;
+            }
+            if (i + 1 === index) {
+               node.nextNode = node.nextNode.nextNode;
+               return;
+            }
+
+            helper(node.nextNode, i + 1);
+         };
+
+         return helper(list.head, 0);
+      }
+   };
+
+   return list;
+};
