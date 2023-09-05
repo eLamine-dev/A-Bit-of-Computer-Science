63e69336b7398952b4434f7b94866513d066dd7c
e-lamine <lamine@e-lamine.dev>
2023-09-05 19:38:46 +0100

    insert function

--- 4-Binary Search Tree/BinarySearchTrees.js
+++ 4-Binary Search Tree/BinarySearchTrees.js
@@ -19,6 +19,21 @@ function Tree(array) {
 
    tree.root = buildTree(sortedArray);
 
+   tree.insert = (value, currentNode = tree.root) => {
+      if (currentNode === null) {
+         currentNode = createNode(value);
+         return currentNode;
+      }
+
+      if (value > currentNode.data) {
+         currentNode.right = tree.insert(value, currentNode.right);
+      } else if (value < currentNode.data) {
+         currentNode.left = tree.insert(value, currentNode.left);
+      }
+
+      return currentNode;
+   };
+
    return tree;
 }
 
@@ -36,4 +51,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 };
 
 let myTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+myTree.insert(6);
+
 prettyPrint(myTree.root);
